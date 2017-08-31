c497a7e33c100d7135383e7d4a1fc493b1a37204
manju <manjupaul1@gmail.com>
2017-08-31 12:49:42 -0400

    code refactoring

---
+++ geocode.js
@@ -0,0 +1,26 @@
+const request=require('request');
+//var address = argv.a;
+
+var getAddress = (address)=> {
+    var encodedAddress = encodeURIComponent(address);
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env['GOOGLE_KEY']}`,
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            console.log("Unable to connect Google server");
+        }
+        else if (body.status == "ZERO_RESULTS") {
+            console.log("Unable to found");
+        }
+        else if (body.status == 'OK') {
+            console.log(`Address:${body.results[0].formatted_address}`);
+            console.log(`Latitude:${body.results[0].geometry.location.lat}`);
+            console.log(`Longitude:${body.results[0].geometry.location.lng}`);
+        }
+        //console.log(body);
+        //console.log(JSON.stringify(body,undefined,2));
+
+    });
+};
+module.exports.getAddress = getAddress;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const yargs = require('yargs');
-
+const geocode =require('./geocode');
 const argv = yargs
     .options({
         a: {
@@ -13,27 +13,4 @@ const argv = yargs
     .help()
     .alias('help', 'h')
     .argv;
-var address = argv.a;
-var encodedAddress = encodeURIComponent(address);
-//console.log('encodedAddress:',encodedAddress);
-//request (options,callback)
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env['GOOGLE_KEY']}`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log("Unable to connect Google server");
-    }
-    else if (body.status == "ZERO_RESULTS") {
-        console.log("Unable to found");
-    }
-    else if (body.status == 'OK') {
-        console.log(`Address:${body.results[0].formatted_address}`);
-        console.log(`Latitude:${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude:${body.results[0].geometry.location.lng}`);
-    }
-    //console.log(body);
-    //console.log(JSON.stringify(body,undefined,2));
-
-})
+geocode.getAddress(argv.a);
