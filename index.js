36d81f9666e0128c8d7a1da8e72f3f15d3219bee
manju <manjupaul1@gmail.com>
2017-08-31 12:03:12 -0400

    added error handling

--- index.js
+++ index.js
@@ -22,9 +22,18 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env['GOOGLE_KEY']}`,
     json: true
 }, (error, response, body) => {
+    if (error) {
+        console.log("Unable to connect Google server");
+    }
+    else if (body.status == "ZERO_RESULTS") {
+        console.log("Unable to found");
+    }
+    else if (body.status == 'OK') {
+        console.log(`Address:${body.results[0].formatted_address}`);
+        console.log(`Latitude:${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude:${body.results[0].geometry.location.lng}`);
+    }
     //console.log(body);
     //console.log(JSON.stringify(body,undefined,2));
-    console.log(`Address:${body.results[0].formatted_address}`);
-    console.log(`Latitude:${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude:${body.results[0].geometry.location.lng}`);
+
 })
