67175faba01e46a610ee7d1e860037a516bc8f35
manju <manjupaul1@gmail.com>
2017-08-31 09:28:32 -0400

    New files

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a:{
+            alias : 'address',
+            demand :true,
+            describe : 'Address to fetch weather',
+            string : true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+
 //request (options,callback)
 
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=herndon+va&key=${process.env['GOOGLE_KEY']}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=14390%20Air%20and%20Space%20Museum%20Parkway%20Chantilly+Virginia+%2020151&key=${process.env['GOOGLE_KEY']}`,
      json :true
 },(error, response, body) =>{
     //console.log(body);
